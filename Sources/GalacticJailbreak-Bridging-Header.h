ea4f0fdd4a1c45255fa5f68ae16adc618f59cd3e
Galactus-byte <Benjamina195@Outlook.com>
2026-08-12 21:16:38 +0000

    fix: correct bridging header import paths

--- Sources/GalacticJailbreak-Bridging-Header.h
+++ Sources/GalacticJailbreak-Bridging-Header.h
@@ -1,11 +1,11 @@
 #ifndef GalacticJailbreak_Bridging_Header_h
 #define GalacticJailbreak_Bridging_Header_h
 
-#import "../../Dopamine/Application/Dopamine/Jailbreak/DOJailbreaker.h"
-#import "../../Dopamine/Application/Dopamine/Jailbreak/DOExploit.h"
-#import "../../Dopamine/Application/Dopamine/Jailbreak/DOExploitManager.h"
-#import "../../Dopamine/Application/Dopamine/Jailbreak/DOBootstrapper.h"
-#import "../../Dopamine/Application/Dopamine/Jailbreak/DOEnvironmentManager.h"
-#import "../../Dopamine/Application/Dopamine/Jailbreak/DOPreferenceManager.h"
+#import "../Dopamine/Application/Dopamine/Jailbreak/DOJailbreaker.h"
+#import "../Dopamine/Application/Dopamine/Jailbreak/DOExploit.h"
+#import "../Dopamine/Application/Dopamine/Jailbreak/DOExploitManager.h"
+#import "../Dopamine/Application/Dopamine/Jailbreak/DOBootstrapper.h"
+#import "../Dopamine/Application/Dopamine/Jailbreak/DOEnvironmentManager.h"
+#import "../Dopamine/Application/Dopamine/Jailbreak/DOPreferenceManager.h"
 
 #endif
